80102250c651086f5c2043b6caa72f2b65c6ca93
Rafael Antunes <rafaelcantunes3@gmail.com>
2025-10-07 20:27:50 -0300

    output de user e senha db

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,12 @@ output "rds_port" {
 output "rds_security_group_id" {
   value = aws_security_group.rds_sg.id
 }
+
+output "db_username" {
+  value = var.db_username
+}
+
+output "db_password" {
+  value = var.db_password
+  sensitive = true
+}
