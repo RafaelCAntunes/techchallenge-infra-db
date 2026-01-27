eb69d345a0063c659f4fb79c6365bfebe71d080b
Rafael Antunes <rafaelcantunes3@gmail.com>
2026-01-27 02:17:22 -0300

    ajustando retenção de backup pra 0 para manter free tier

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_db_instance" "mysql" {
   skip_final_snapshot     = true
   publicly_accessible     = false
   multi_az                = false
-  backup_retention_period = 7
+  backup_retention_period = 0
 
   tags = {
     Name = "techchallenge-mysql"
