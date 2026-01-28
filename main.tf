02c06d14eed26ce8a95c0e4037164a639e6f0f57
Rafael Antunes <rafaelcantunes3@gmail.com>
2026-01-28 00:21:15 -0300

    adicionando nome do banco na criação da instancia

--- main.tf
+++ main.tf
@@ -58,6 +58,7 @@ resource "aws_db_instance" "mysql" {
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   storage_type            = "gp2"
+  db_name                 = "lanchonete"
   username                = var.db_username
   password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.rds_subnets.name
